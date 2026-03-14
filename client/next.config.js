f21c279d9b86aeb33ec1ee21e900eebceccbfb73
Icecubesaad <saad.qad418@gmail.com>
2026-03-14 17:39:48 +0500

    boom

--- client/next.config.js
+++ client/next.config.js
@@ -4,101 +4,103 @@ module.exports = {
     domains: ['res.cloudinary.com'],
   },
   async rewrites() {
+    const SERVER_URL = process.env.SERVER_URL || 'http://localhost:5000';
+    
     return [
       {
         source: "/api/post/checkUser",
-        destination: "http://localhost:5000/api/post/checkUser",
+        destination: `${SERVER_URL}/api/post/checkUser`,
       },
       {
         source:"/api/post/checkusername",
-        destination:"http://localhost:5000/api/post/checkusername"
+        destination:`${SERVER_URL}/api/post/checkusername`
       }
       ,
       {
         source:"/api/post/Register",
-        destination:"http://localhost:5000/api/post/Register"
+        destination:`${SERVER_URL}/api/post/Register`
       },
       {
         source:"/api/post/login",
-        destination:"http://localhost:5000/api/post/login"
+        destination:`${SERVER_URL}/api/post/login`
       }
       ,
       {
         source:"/api/get/getUser",
-        destination:"http://localhost:5000/api/get/getUser"
+        destination:`${SERVER_URL}/api/get/getUser`
       },
       {
         source:"/api/post/tweet",
-        destination:"http://localhost:5000/api/post/tweet"
+        destination:`${SERVER_URL}/api/post/tweet`
       },
       {
         source:"/api/post/comment/:id",
-        destination:"http://localhost:5000/api/post/comment/:id"
+        destination:`${SERVER_URL}/api/post/comment/:id`
       },
       {
         source:"/api/get/getComments/:id",
-        destination:"http://localhost:5000/api/get/getComments/:id"
+        destination:`${SERVER_URL}/api/get/getComments/:id`
       },
       {
         source:"/api/get/getAllTweet",
-        destination:"http://localhost:5000/api/get/getAllTweet"
+        destination:`${SERVER_URL}/api/get/getAllTweet`
       },
       {
         source:"/api/get/getTweet/:id",
-        destination:"http://localhost:5000/api/get/getTweet/:id"
+        destination:`${SERVER_URL}/api/get/getTweet/:id`
       },
       {
         source:"/api/get/getComment/:id",
-        destination:"http://localhost:5000/api/get/getComment/:id"
+        destination:`${SERVER_URL}/api/get/getComment/:id`
       },
       {
         source:"/api/post/like/:id/:type",
-        destination:"http://localhost:5000/api/post/like/:id/:type"
+        destination:`${SERVER_URL}/api/post/like/:id/:type`
       }
       ,
       {
         source:"/api/get/whoToFollow",
-        destination:"http://localhost:5000/api/get/whoToFollow"
+        destination:`${SERVER_URL}/api/get/whoToFollow`
       }
       ,
       {
         source:"/api/get/fetchSingleUserDetails/:id",
-        destination:"http://localhost:5000/api/get/getSingleUser/:id"
+        destination:`${SERVER_URL}/api/get/getSingleUser/:id`
       }
       ,
       {
         source:"/api/get/fetchUserTweets/:id",
-        destination:"http://localhost:5000/api/get/getSingleUserTweets/:id"
+        destination:`${SERVER_URL}/api/get/getSingleUserTweets/:id`
       }
       ,
       {
         source:"/api/get/fetchUserReplies/:id",
-        destination:"http://localhost:5000/api/get/getSingleUserComments/:id"
+        destination:`${SERVER_URL}/api/get/getSingleUserComments/:id`
       }
       ,
       {
         source:"/api/get/fetchUserLikes/:id",
-        destination:"http://localhost:5000/api/get/getSingleUserLikes/:id"
+        destination:`${SERVER_URL}/api/get/getSingleUserLikes/:id`
       }
       ,
       {
         source:"/api/post/follow/:id/:follow",
-        destination:"http://localhost:5000/api/post/follow/:id/:follow"
+        destination:`${SERVER_URL}/api/post/follow/:id/:follow`
       }
       ,
       {
         source:"/api/get/fetchFollowers/:id",
-        destination:"http://localhost:5000/api/get/getSingleUserFollowers/:id"
+        destination:`${SERVER_URL}/api/get/getSingleUserFollowers/:id`
       }
       ,
       {
         source:"/api/get/fetchFollowing/:id",
-        destination:"http://localhost:5000/api/get/getSingleUserFollowing/:id"
+        destination:`${SERVER_URL}/api/get/getSingleUserFollowing/:id`
       }
       ,
       {
         source:"/api/get/fetchFollowing",
-        destination:"http://localhost:5000/api/get/followingFeed"
+        destination:`${SERVER_URL}/api/get/followingFeed`
       }
     ]
   },
